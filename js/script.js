ebba8211529bcb094b11374fce056e0f90d92532
kentakang <kentakang@icloud.com>
2016-04-23 02:36:47 +0900

    Update Portfolio Function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
 	$('.portfolio').hide();
 	$('#call_portfolio').click(function(){
+		$('.portfolio').removeClass('animated slideOutLeft');
 		$('.portfolio').show();
 		$('.portfolio').addClass('animated slideInLeft');
 		$('body').addClass('blur');
+	});
+	$('.close_btn').click(function(){
+		$('.portfolio').removeClass('animated slideInLeft');
+		$('.portfolio').addClass('animated slideOutLeft');
+		$('body').removeClass('blur');
 	});
 });
