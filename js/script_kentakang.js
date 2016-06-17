bdebb19b2a3c0f67aff27e1399d5119d69403068
kentakang <kentakang@icloud.com>
2016-06-18 02:45:16 +0900

    ref #2 Responsive Design Change a Background Image and Add OSS Notice.

---
+++ js/script_kentakang.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('.portfolio').hide();
+	$('#call_portfolio').click(function(){
+		$('#portfolio').removeClass('animated slideOutLeft');
+		$('#portfolio').show();
+		$('#portfolio').addClass('animated slideInLeft');
+		$('body').addClass('blur');
+	});
+  $('#call_license').click(function(){
+		$('#license').removeClass('animated slideOutLeft');
+		$('#license').show();
+		$('#license').addClass('animated slideInLeft');
+		$('body').addClass('blur');
+	});
+	$('.close_btn').click(function(){
+		$('.portfolio').removeClass('animated slideInLeft');
+		$('.portfolio').addClass('animated slideOutLeft');
+		$('body').removeClass('blur');
+	});
+});
