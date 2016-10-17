141f568b5b9b62ef9baa3457528e67363698ddbb
kentakang <kentakang@icloud.com>
2016-10-17 19:27:19 +0900

    ref #2 add background click event

--- js/script_kentakang.js
+++ js/script_kentakang.js
@@ -5,16 +5,25 @@ $(document).ready(function(){
 		$('#portfolio').show();
 		$('#portfolio').addClass('animated slideInLeft');
 		$('body').addClass('blur');
+		$('#curtain').addClass('curtain');
 	});
-  $('#call_license').click(function(){
+	$('#call_license').click(function(){
 		$('#license').removeClass('animated slideOutLeft');
 		$('#license').show();
 		$('#license').addClass('animated slideInLeft');
 		$('body').addClass('blur');
+		$('#curtain').addClass('curtain');
 	});
 	$('.close_btn').click(function(){
 		$('.portfolio').removeClass('animated slideInLeft');
 		$('.portfolio').addClass('animated slideOutLeft');
 		$('body').removeClass('blur');
+		$('#curtain').removeClass('curtain');
+	});
+	$('#curtain').click(function(){
+		$('.portfolio').removeClass('animated slideInLeft');
+		$('.portfolio').addClass('animated slideOutLeft');
+		$('body').removeClass('blur');
+		$('#curtain').removeClass('curtain');
 	});
 });
